1e414aa75d783c1a23634f96dd0b483e2d7d209e
Dmitriy <dspv@me.com>
2022-02-16 17:10:59 +0200

    Add variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable vpc_id {}
+variable account {}
 variable account_id {}
 variable subnets {}
 variable image_name {}
@@ -6,8 +7,17 @@ variable logdna_key {}
 variable domain_name {}
 variable name_prefix {}
 variable aws_lb_arn {}
+variable aws_region {}
 variable ecr_region {}
 variable ecr_account_id {}
+variable standard_tags {}
 variable security_groups {}
 variable execution_role_arn {}
+variable container_insights {}
 variable aws_lb_certificate_arn {}
+
+variable instance_types {}
+variable cluster_sg {}
+variable ecs_subnet {}
+variable ebs_disks {}
+variable key_name {}
