209f6dbe6b3571e6dd8441887903de67f3b6dc62
Dmitriy <dspv@me.com>
2022-02-16 16:56:12 +0200

    Add more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
+output "cluster_name" {
+    description = "ECS Cluster Name"
+    value       = module.ecs_cluster.cluster_name
+}
+output "cluster_arn" {
+    description = "ECS Cluster ARN"
+    value       = module.ecs_cluster.cluster_arn
+}
+output "capacity_provider_name" {
+    description = "ECS Cluster Capacity Provider"
+    value       = module.ecs_cluster.cluster_capacity_provider
+}
+
+
 output billing_port {
     value = var.service_config.billing.port
 }
