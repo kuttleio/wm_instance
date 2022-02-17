0733776bd7812b56e837f396cf82eb83b3215b1f
Dmitriy <dspv@me.com>
2022-02-17 12:06:59 +0200

    Add all 16 wematch backends + server

--- outputs.tf
+++ outputs.tf
@@ -12,14 +12,51 @@ output "cluster_capacity_provider" {
 }
 
 
-output billing_port {
+output server {
+    value = var.service_config.server.port
+}
+output billing {
     value = var.service_config.billing.port
 }
-
-output marketdata_port {
+output market_stats_collector {
+    value = var.service_config.market_stats_collector.port
+}
+output marketdata {
     value = var.service_config.marketdata.port
 }
-
-output nego_port {
+output match_negotiations {
+    value = var.service_config.match_negotiations.port
+}
+output matching {
+    value = var.service_config.matching.port
+}
+output nego {
     value = var.service_config.nego.port
+}
+output nego_client {
+    value = var.service_config.nego_client.port
+}
+output optimizer {
+    value = var.service_config.optimizer.port
+}
+output other {
+    value = var.service_config.other.port
+}
+output positions {
+    value = var.service_config.positions.port
+}
+output refinitiv_ingestion {
+    value = var.service_config.refinitiv_ingestion.port
+}
+output requests {
+    value = var.service_config.requests.port
+}
+output sales {
+    value = var.service_config.sales.port
+}
+output speech {
+    value = var.service_config.speech.port
+}
+output uploader {
+    value = var.service_config.uploader.port
 }
