5c394b3888cf08a04965b032ec252869420709da
Dmitriy Solodukha <dspv@me.com>
2022-02-21 12:44:36 +0200

    Add desired count to turn-on, turn-off at once (#5)
    * Add desired count to turn-on, turn-off at once
    * Update module: 1.0.3 -> 1.0.4
    * Update module: 1.0.4 -> 1.0.5

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module ecs_cluster {
 #    Services
 # ---------------------------------------------------
 module server {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -33,6 +33,7 @@ module server {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.server.service_name
     image_name              = var.service_config.server.image_name
     image_version           = var.service_config.server.image_version
@@ -41,7 +42,7 @@ module server {
 }
 
 module admin {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -57,6 +58,7 @@ module admin {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.admin.service_name
     image_name              = var.service_config.admin.image_name
     image_version           = var.service_config.admin.image_version
@@ -65,7 +67,7 @@ module admin {
 }
 
 module client {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -81,6 +83,7 @@ module client {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.client.service_name
     image_name              = var.service_config.client.image_name
     image_version           = var.service_config.client.image_version
@@ -90,7 +93,7 @@ module client {
 }
 
 module billing {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -106,6 +109,7 @@ module billing {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.billing.service_name
     image_name              = var.service_config.billing.image_name
     image_version           = var.service_config.billing.image_version
@@ -113,7 +117,7 @@ module billing {
 }
 
 module market_stats_collector {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -129,6 +133,7 @@ module market_stats_collector {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.market_stats_collector.service_name
     image_name              = var.service_config.market_stats_collector.image_name
     image_version           = var.service_config.market_stats_collector.image_version
@@ -136,7 +141,7 @@ module market_stats_collector {
 }
 
 module marketdata {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -152,6 +157,7 @@ module marketdata {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.marketdata.service_name
     image_name              = var.service_config.marketdata.image_name
     image_version           = var.service_config.marketdata.image_version
@@ -159,7 +165,7 @@ module marketdata {
 }
 
 module match_negotiations {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -175,6 +181,7 @@ module match_negotiations {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.match_negotiations.service_name
     image_name              = var.service_config.match_negotiations.image_name
     image_version           = var.service_config.match_negotiations.image_version
@@ -182,7 +189,7 @@ module match_negotiations {
 }
 
 module matching {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -198,6 +205,7 @@ module matching {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.matching.service_name
     image_name              = var.service_config.matching.image_name
     image_version           = var.service_config.matching.image_version
@@ -205,7 +213,7 @@ module matching {
 }
 
 module nego {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -221,6 +229,7 @@ module nego {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.nego.service_name
     image_name              = var.service_config.nego.image_name
     image_version           = var.service_config.nego.image_version
@@ -228,7 +237,7 @@ module nego {
 }
 
 module nego_client {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -244,6 +253,7 @@ module nego_client {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.nego_client.service_name
     image_name              = var.service_config.nego_client.image_name
     image_version           = var.service_config.nego_client.image_version
@@ -251,7 +261,7 @@ module nego_client {
 }
 
 module optimizer {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -267,6 +277,7 @@ module optimizer {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.optimizer.service_name
     image_name              = var.service_config.optimizer.image_name
     image_version           = var.service_config.optimizer.image_version
@@ -274,7 +285,7 @@ module optimizer {
 }
 
 module other {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -290,6 +301,7 @@ module other {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.other.service_name
     image_name              = var.service_config.other.image_name
     image_version           = var.service_config.other.image_version
@@ -297,7 +309,7 @@ module other {
 }
 
 module positions {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -313,6 +325,7 @@ module positions {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.positions.service_name
     image_name              = var.service_config.positions.image_name
     image_version           = var.service_config.positions.image_version
@@ -320,7 +333,7 @@ module positions {
 }
 
 module refinitiv_ingestion {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -336,6 +349,7 @@ module refinitiv_ingestion {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.refinitiv_ingestion.service_name
     image_name              = var.service_config.refinitiv_ingestion.image_name
     image_version           = var.service_config.refinitiv_ingestion.image_version
@@ -343,7 +357,7 @@ module refinitiv_ingestion {
 }
 
 module requests {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -359,6 +373,7 @@ module requests {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.requests.service_name
     image_name              = var.service_config.requests.image_name
     image_version           = var.service_config.requests.image_version
@@ -366,7 +381,7 @@ module requests {
 }
 
 module sales {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -382,6 +397,7 @@ module sales {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.sales.service_name
     image_name              = var.service_config.sales.image_name
     image_version           = var.service_config.sales.image_version
@@ -389,7 +405,7 @@ module sales {
 }
 
 module speech {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -405,6 +421,7 @@ module speech {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.speech.service_name
     image_name              = var.service_config.speech.image_name
     image_version           = var.service_config.speech.image_version
@@ -412,7 +429,7 @@ module speech {
 }
 
 module uploader {
-    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.3"
+    source                  = "github.com/zbs-nu/aws_ecs_service//?ref=1.0.5"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -428,6 +445,7 @@ module uploader {
     aws_lb_certificate_arn  = var.aws_lb_certificate_arn
     domain_name             = var.domain_name    
     environment             = var.environment
+    desired_count           = var.desired_count
     service_name            = var.service_config.uploader.service_name
     image_name              = var.service_config.uploader.image_name
     image_version           = var.service_config.uploader.image_version
