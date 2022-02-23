5dcd40a3a7f2a46b5c297f7cc3a733645b7865e6
Dmitriy Solodukha <dspv@me.com>
2022-02-23 10:24:53 +0200

    Change module version for public services (#10)

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module ecs_cluster {
 #    Services
 # ---------------------------------------------------
 module server {
-    source                  = "github.com/zbs-nu/aws_ecs_service_public//?ref=initial-commit-2022-02-22"
+    source                  = "github.com/zbs-nu/aws_ecs_service_public//"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -42,7 +42,7 @@ module server {
 }
 
 module admin {
-    source                  = "github.com/zbs-nu/aws_ecs_service_public//?ref=initial-commit-2022-02-22"
+    source                  = "github.com/zbs-nu/aws_ecs_service_public//"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
@@ -67,7 +67,7 @@ module admin {
 }
 
 module client {
-    source                  = "github.com/zbs-nu/aws_ecs_service_public//?ref=initial-commit-2022-02-22"
+    source                  = "github.com/zbs-nu/aws_ecs_service_public//"
     name_prefix             = var.name_prefix
     standard_tags           = var.standard_tags
     cluster_name            = module.ecs_cluster.cluster_name
